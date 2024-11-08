f70a36ca2c9b88c3ca5f966d215c0c860cf0a2d6
janaampova <ampova.jana29@gmail.com>
2024-11-08 14:48:38 +0200

    editSingle

--- home.php
+++ home.php
@@ -1,47 +1,155 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TJ EasyStay</title>
-    <link rel="stylesheet" href="logIn.css">
     <link rel="stylesheet" href="css/home.css">
 </head>
+
 <body>
-    <div class="navbar">
-        <div class="menu">
-            <ul>
-                <li><a href="#info">Info</a></li>
-                <li><a href="#help">Help</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li><a href="#signUp">Sign Up</a></li>
-                <li><a href="#logIn">Log In</a></li>
-            </ul>
+    <div class="main" id="main">
+        <div class="icon">
+            <a href="#main" class="logo">
+                <h3>TJ</h3>
+                <p>
+                    <h6>EasyStay</h6>
+                </p>
+            </a>
+
+        </div>
+
+
+        <div class="homeText">
+            <h1> Explore Bulgaria! <br> Easier with us!</h1>
+            <br><br>
+
+        </div>
+
+        <div class="navbar">
+            <div class="menu">
+                <ul>
+                    <li><a href="#info">About us</a></li>
+                    <li><a href="#help">Help</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#signUp">Sign Up</a></li>
+                    <li><a href="#logIn">Log In</a></li>
+                </ul>
+            </div>
         </div>
     </div>
-    
-    <form action="" method="POST">
-        <h2 class="greetings">Welcome!</h2>
-    </form>
 
-    <section id="info">
-        <p>Info content with a background image</p>
-    </section>
 
-    <section id="help">
-        <p>Help content</p>
-    </section>
+    <div>
 
-    <section id="signUp">
-        <div class="links">
-            <a href="userEntry/signUp.php" class="button">Sign Up</a>
+        <section id="info">
+            <div class="content">
+
+                <div class="info-content">
+                    <div class="row">
+
+                        <div class="section-title">
+                            <h1> About us</h1>
+                        </div>
+                    </div>
+
+                    <p>Welcome to EasyStay, your go-to platform for hassle-free hotel bookings! We are two enthusiastic
+                        students, <strong>Jana Ampova</strong> and <strong>Teodora Stoycheva</strong>, who share a
+                        passion for travel and technology. Our journey began during our time at university, where we
+                        recognized the challenges faced by travelers in finding the perfect accommodation.</p>
+
+                    <p>With our combined skills in web development and design, we set out to create an intuitive website
+                        that simplifies the hotel booking process. At EasyStay, we believe that everyone deserves a
+                        seamless experience when planning their adventures. Our user-friendly interface allows you to
+                        explore a wide range of hotels, compare prices, and read genuine reviews—all in one place.</p>
+
+                    <p>Driven by a commitment to exceptional service, we are dedicated to helping you find the ideal
+                        stay that fits your budget and preferences. Whether you're traveling for business, leisure, or a
+                        special occasion, EasyStay is here to make your hotel booking experience enjoyable and
+                        straightforward.</p>
+
+                    <p><b>Thank you for choosing EasyStay. We look forward to being part of your travel journey!</b></p>
+
+
+                    <div class="stats">
+                        <div class="row">
+                            <div class="stat-box">
+                                <h2>150+</h2>
+                                <h5>Properties</h5>
+                            </div>
+                            <div class="stat-box">
+                                <h2>500+</h2>
+                                <h5>Hosts</h5>
+                            </div>
+                            <div class="stat-box">
+                                <h2>1К+</h2>
+                                <h5>Clients</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+
+        <section id="help">
+            <div class="content">
+                <p>Help content</p>
+            </div>
+        </section>
+
+        <section >
+            <div class="content" id="signUp">
+                <div class="info-content">
+                    <div class="row">
+                        <div class="section-title">
+                            <h1> Sign up Today </h1>
+                        </div>
+                    </div>
+                    <p>Join our community and unlock exclusive access to incredible hotel deals, personalized
+                        recommendations, a seamless booking experience and the chance to connect with travelers from
+                        around the world.
+                        With <b>EasyStay</b>, your perfect getaway is just <u><i>click</i></u> away!</p>
+                    <p>Whether you're a traveler searching for your next adventure or a host ready to share your
+                        unique
+                        property, signing up opens the door to endless possibilities.
+                        Your journey starts here, let's make it unforgettable together!</p>
+                        <p><b>Click the link below to start!</b></p>
+
+
+                    <div class="links">
+                        <a href="userEntry/signUp.php" class="button">Sign Up</a>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+        <div class="">
+
         </div>
-    </section>
 
-    <section id="logIn">
-        <div class="links">
-            <a href="userEntry/logIn.php" class="button">Log In</a>
-        </div>
-    </section>
+        <section >
+            <div class="content" id="logIn">
+                <div class="info-content">
+                    <div class="row">
+                        <div class="section-title">
+                            <h1> Welcome back </h1>
+                        </div>
+                    </div>
+                    <p>Welcome back to <b>EasyStay!</b> Log in to access your personalized dashboard and
+                        explore new destinations.Your adventure continues, let’s make
+                        the most of it!</p>
+                        <p><b>Click the link below and enjoy!</b></p>
+                    <div class="links">
+                        <a href="userEntry/logIn.php" class="button">Log In</a>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    </div>
+
 </body>
+
 </html>
